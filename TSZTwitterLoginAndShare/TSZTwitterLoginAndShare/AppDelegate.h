849b12e97ca9628502a04b37b5ca96524780de76
tang <tangdream@163.com>
2016-03-08 17:42:04 +0800

    init  app

---
+++ TSZTwitterLoginAndShare/TSZTwitterLoginAndShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSZTwitterLoginAndShare
+//
+//  Created by tang on 16/3/8.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
